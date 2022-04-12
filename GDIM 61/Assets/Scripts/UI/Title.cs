18a57410d0bec0453949919a1390e1a647c9b1b1
Esteban Aldana <estebanaaldana@gmail.com>
2022-04-12 14:41:47 -0700

    Title Screen changes scene

--- GDIM 61/Assets/Scripts/UI/Title.cs
+++ GDIM 61/Assets/Scripts/UI/Title.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
     public Animator anim;
+    public string nextScene = "";
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         
+    }
+
+    public void ActivateStart()
+    {
+        if(nextScene!="")
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+
     }
 
     public void ActivateCredits()
