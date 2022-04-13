c2f2b206a2f1dd340db3bd4779f3123e686b5e5b
Amber <ahmborr@gmail.com>
2022-04-12 20:46:19 -0700

    mini interactable progress bar
    style will be changed later based on the house vibe

---
+++ GDIM 61/Assets/Scripts/UI/ProgressBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ProgressBar : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI percentage;
+
+    public void onSliderChanged(float value)
+    {
+        percentage.text = value.ToString();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
