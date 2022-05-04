a580a559fbf30bfca7e09f5c8bdac32a919883f8
jdn17 <nheanj@uci.edu>
2022-05-04 13:26:46 -0700

    Fear Bar Prototype

---
+++ GDIM 61/Assets/Scripts/Josh/FearBar.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FearBar : MonoBehaviour
+{
+    [SerializeField] GameObject ai;
+    private HumanFOV detection;
+
+    [SerializeField] private float scale = 0.001f;
+    private float max = 1f;
+    private float min = 0f;
+    private float newX;
+    
+    void Start()
+    {
+        detection = ai.GetComponent<HumanFOV>();
+    }
+
+    void Update()
+    {
+        FillBar();
+    }
+
+    void FillBar()
+    {
+        if (detection.detectPlayer)
+        {
+            if (transform.localScale.x <= max)
+            {
+                transform.localScale = new Vector3(transform.localScale.x + scale, transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                transform.localScale = new Vector3(max, transform.localScale.y, transform.localScale.z);
+            }
+            
+        }
+        else
+        {
+            if (transform.localScale.x >= min)
+            {
+                transform.localScale = new Vector3(transform.localScale.x - scale, transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                transform.localScale = new Vector3(min, transform.localScale.y, transform.localScale.z);
+            }
+
+        }
+    }
+
+}
